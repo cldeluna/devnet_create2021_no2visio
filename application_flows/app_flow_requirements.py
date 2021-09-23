fe698b69af00f740e913aefbfa154ea58f95dd66
Claudia de Luna <cldeluna@yahoo.com>
2021-09-23 06:16:13 -0700

    Added options for printing IPs and for Verbose output

--- application_flows/app_flow_requirements.py
+++ application_flows/app_flow_requirements.py
@@ -13,6 +13,10 @@ __copyright__ = "Copyright (c) 2018 Claudia"
 __license__ = "Python"
 
 import argparse
+import base64
+import requests, io
+from PIL import Image
+import matplotlib.pyplot as plt
 import pandas as pd
 
 
@@ -20,35 +24,43 @@ def main():
 
     # Use Pandas to read in an Excel file
 
+    include_ips = arguments.include_ips
+    verbose = arguments.verbose
+
     excel_file = 'base_station2tpe_connreq.xlsx'
+    print(f"\nReading into Pandas DataFrame Excel file containing Application Flow Requirements:\n{excel_file}")
     df = pd.read_excel(excel_file)
     df.fillna('', inplace=True)
 
-    print(df)
+    if verbose:
+        print(f"Example of DataFrame:\n{df.head}")
 
+    # Turn dataframe into List of Dictionaries (lod)
     policy_lod = df.to_dict(orient="records")
 
-    include_ips = False
+    # Initialize a list to hold each line of the Mermaid Diagram
     sequence_diagram = []
 
-    nodes = set()
-    src_nodes = set()
-    dst_nodes = set()
-
-    for line in policy_lod:
-        print(line)
-
-        if line['From System'] not in src_nodes:
-            src_nodes.add(line['From System'])
-            nodes.add(line['From System'])
-
-        if line['To System'] not in dst_nodes:
-            dst_nodes.add(line['To System'])
-            nodes.add(line['To System'])
-
-    print(src_nodes)
-    print(dst_nodes)
-    print(nodes)
+    # nodes = set()
+    # src_nodes = set()
+    # dst_nodes = set()
+    #
+    # # Interate through the lod and establish Nodes
+    # for line in policy_lod:
+    #     if verbose:
+    #         print(line)
+    #
+    #     if line['From System'] not in src_nodes:
+    #         src_nodes.add(line['From System'])
+    #         nodes.add(line['From System'])
+    #
+    #     if line['To System'] not in dst_nodes:
+    #         dst_nodes.add(line['To System'])
+    #         nodes.add(line['To System'])
+    #
+    # print(src_nodes)
+    # print(dst_nodes)
+    # print(nodes)
 
     sequence_diagram.append("sequenceDiagram")
 
@@ -72,10 +84,18 @@ def main():
         print(line)
 
 
-
 # Standard call to the main() function.
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Script Description",
                                      epilog="Usage: ' python app_flow_requirements' ")
+    parser.add_argument('-i', '--include_ips',
+                        help='Show IPs in Sequence diagram',
+                        action='store_true',
+                        default=False)
+
+    parser.add_argument('-v', '--verbose',
+                        help='Print values along the way',
+                        action='store_true',
+                        default=False)
     arguments = parser.parse_args()
     main()
